d2e3b56d680d6b6ebc28f7e85ec58b3af94ff6d1
dcastor <dcastor@student.42.fr>
2025-06-12 11:46:13 +0200

    chore: gitignore updated

---
+++ srcs/init/init_env.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 09:42:38 by dcastor           #+#    #+#             */
+/*   Updated: 2025/06/12 11:10:57 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	init_env(t_app *app, char *envp[])
+{
+	// t_env	*new_env;
+	(void)app;
+	(void)envp;
+	// while (envp && *envp)
+	// {
+	//     gc
+	// 	printf("env\n");
+	// 	envp++;
+	// }
+}
+
+// static void	add_env_back(t_env **env_list, char)
