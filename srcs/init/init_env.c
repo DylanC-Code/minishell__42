725625ad0e6187d7ac601490d70bed7aed4b8569
dcastor <dcastor@student.42.fr>
2025-06-12 12:47:16 +0200

    feat(env): inherited env variables added to t_env list

--- srcs/init/init_env.c
+++ srcs/init/init_env.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 09:42:38 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/12 11:50:36 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/12 12:43:09 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,22 @@
 
 void	init_env(t_app *app, char *envp[])
 {
+	char	*p_delimiter;
 	t_env	*new_env;
 
 	while (envp && *envp)
 	{
 		new_env = gc_malloc(sizeof(t_env), &app->garb_head);
-		// printf("env\n");
+		p_delimiter = ft_strchr(*envp, '=');
+		new_env->key = ft_strndup(*envp, p_delimiter - *envp);
+		if (!new_env->key)
+			exit_with_error("malloc", &app->garb_head);
+		add_to_gc(&app->garb_head, new_env->key);
+		new_env->value = ft_strdup(p_delimiter + 1);
+		if (!new_env->value)
+			exit_with_error("malloc", &app->garb_head);
+		add_to_gc(&app->garb_head, new_env->value);
+		printf("%s\n", new_env->key);
 		envp++;
 	}
 }
