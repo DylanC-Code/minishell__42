7dc8c0a880f1ea31c3823b39bd8afc9064d6a806
dcastor <dcastor@student.42.fr>
2025-06-14 15:08:43 +0200

    fix(tokenizer): operator type inference based on token.value instead of str

--- srcs/input/tokenizer/token_operator.c
+++ srcs/input/tokenizer/token_operator.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 11:48:42 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/14 12:16:01 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/14 15:06:13 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,51 +17,51 @@
 /* =============== Declaration =============== */
 
 void		handle_operator_token(t_token *token, char *str);
-static void	handle_operator_token_type(t_token *token, char *str);
-static void	handle_single_char_operator(t_token *token, char *operator);
-static void	handle_double_char_operator(t_token *token, char *operator);
+static void	handle_operator_token_type(t_token *token);
+static void	handle_single_char_operator(t_token *token);
+static void	handle_double_char_operator(t_token *token);
 
 /* =============== Definition ================ */
 
 void	handle_operator_token(t_token *token, char *str)
 {
-	handle_operator_token_type(token, str);
 	if (str[0] != str[1])
 		token->value = ft_strndup(str, 1);
 	else
 		token->value = ft_strndup(str, 2);
+	handle_operator_token_type(token);
 }
 
-static void	handle_operator_token_type(t_token *token, char *str)
+static void	handle_operator_token_type(t_token *token)
 {
-	if (ft_strlen(str) == 1)
-		handle_single_char_operator(token, str);
-	else if (ft_strlen(str) == 2)
-		handle_double_char_operator(token, str);
+	if (ft_strlen(token->value) == 1)
+		handle_single_char_operator(token);
+	else if (ft_strlen(token->value) == 2)
+		handle_double_char_operator(token);
 }
 
-static void	handle_single_char_operator(t_token *token, char *operator)
+static void	handle_single_char_operator(t_token *token)
 {
-	if (*operator == '|')
+	if (*token->value == '|')
 		token->type = TOKEN_PIPE;
-	else if (*operator == '<')
+	else if (*token->value == '<')
 		token->type = TOKEN_REDIR_IN;
-	else if (*operator == '>')
+	else if (*token->value == '>')
 		token->type = TOKEN_REDIR_OUT;
-	else if (*operator == '(')
+	else if (*token->value == '(')
 		token->type = TOKEN_OPEN_PARENTHESE;
-	else if (*operator == ')')
+	else if (*token->value == ')')
 		token->type = TOKEN_CLOSE_PARENTHESE;
 }
 
-static void	handle_double_char_operator(t_token *token, char *operator)
+static void	handle_double_char_operator(t_token *token)
 {
-	if (*operator == '|')
+	if (*token->value == '|')
 		token->type = TOKEN_OR;
-	else if (*operator == '<')
+	else if (*token->value == '<')
 		token->type = TOKEN_REDIR_APPEND;
-	else if (*operator == '>')
+	else if (*token->value == '>')
 		token->type = TOKEN_REDIR_HEREDOC;
-	else if (*operator == '&')
+	else if (*token->value == '&')
 		token->type = TOKEN_AND;
 }
