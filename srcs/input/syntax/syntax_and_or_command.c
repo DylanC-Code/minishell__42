52ef102b1fe258afa299b97599eeb9524a7ba356
DylanC-Code <dylanc.code@gmail.com>
2025-06-16 17:00:26 +0200

    feat(syntax): and_or_command error displayed

--- srcs/input/syntax/syntax_and_or_command.c
+++ srcs/input/syntax/syntax_and_or_command.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 15:18:44 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/15 15:26:24 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/16 16:56:33 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,20 @@ static bool	is_and_or_token(t_token *token);
 t_status	syntax_handle_and_or_command(t_token **token_list)
 {
 	t_status	pipeline_handled;
+	t_token		*op_token;
 
+	op_token = *token_list;
+	if (is_and_or_token(*token_list))
+		return (print_syntax_error((*token_list)->value), ERROR);
 	pipeline_handled = syntax_handle_pipeline(token_list);
-	if (pipeline_handled == ERROR)
-		return (ERROR);
-	if (pipeline_handled == NOOP)
-		return (NOOP);
-	while (is_and_or_token(*token_list))
+	if (pipeline_handled != SUCCESS)
+		return (pipeline_handled);
+	while (*token_list && is_and_or_token(*token_list))
 	{
+		op_token = *token_list;
 		*token_list = (*token_list)->next;
+		if (!*token_list || is_and_or_token(*token_list))
+			return (print_syntax_error(op_token->value), ERROR);
 		pipeline_handled = syntax_handle_pipeline(token_list);
 		if (pipeline_handled != SUCCESS)
 			return (ERROR);
--- srcs/utils/error.c
+++ srcs/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 11:55:49 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/12 11:58:00 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/16 16:04:09 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,28 @@ void	exit_with_error(char *msg, t_garbage **garbage_list)
 	perror(msg);
 	gc_cleanup(garbage_list);
 	exit(EXIT_FAILURE); // TODO: adapt status number
+}
+
+t_parse_status	print_syntax_error(const char *token)
+{
+	printf("minishell: syntax error near unexpected token `%s\n", token);
+	return (CMD_INVALID);
+}
+
+t_parse_status	print_op_start_syntax_error(const char *line)
+{
+	while (*line && ft_isspace(*line))
+		line++;
+	if (line[0] == '|' && line[1] == '|')
+		ft_putstr_fd("minishell: syntax error near unexpected token `||'\n", 2);
+	else if (line[0] == '&' && line[1] == '&')
+		ft_putstr_fd("minishell: syntax error near unexpected token `&&'\n", 2);
+	else if (line[0] == '|')
+		ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", 2);
+	else if (line[0] == '&')
+		ft_putstr_fd("minishell: syntax error near unexpected token `&'\n", 2);
+	else
+		ft_putstr_fd("minishell: syntax error near unexpected token `newline'\n",
+			2);
+	return (CMD_INVALID);
 }
