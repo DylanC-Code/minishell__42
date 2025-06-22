a21197cd15dc4959274b1aac9da7d1f9d9b1b5ec
DylanC-Code <dylanc.code@gmail.com>
2025-06-22 09:55:08 +0200

    feat(redirection): implement redirection handling including heredoc support

---
+++ srcs/executor/redirection.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 11:27:39 by dcastor           #+#    #+#             */
+/*   Updated: 2025/06/22 09:27:05 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	handle_redir_in(t_cmd *cmd, t_redir_list *redir);
+static void	handle_redir_out(t_cmd *cmd, t_redir_list *redir);
+static void	handle_redir_append(t_cmd *cmd, t_redir_list *redir);
+
+void	resolve_all_redirection(t_cmd_sequence *seq)
+{
+	t_redir_list	*redir;
+	t_cmd			*cmd;
+
+	while (seq)
+	{
+		cmd = seq->cmds;
+		while (cmd)
+		{
+			redir = cmd->redir_list;
+			while (redir)
+			{
+				handle_redir_in(cmd, redir);
+				handle_redir_out(cmd, redir);
+				handle_redir_append(cmd, redir);
+				if (redir->type == TOKEN_REDIR_HEREDOC)
+				{
+					safe_close(&cmd->fd_in);
+					cmd->fd_in = redir->fd;
+				}
+				redir = redir->next;
+			}
+			cmd = cmd->next;
+		}
+		seq = seq->next;
+	}
+}
+
+static void	handle_redir_in(t_cmd *cmd, t_redir_list *redir)
+{
+	if (redir->type != TOKEN_REDIR_IN)
+		return ;
+	redir->fd = open(redir->name, O_RDONLY);
+	if (redir->fd < 0)
+	{
+		cmd->failed = true;
+		return (perror(redir->name));
+	}
+	safe_close(&cmd->fd_in);
+	cmd->fd_in = redir->fd;
+}
+
+static void	handle_redir_out(t_cmd *cmd, t_redir_list *redir)
+{
+	if (redir->type != TOKEN_REDIR_OUT)
+		return ;
+	redir->fd = open(redir->name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (redir->fd < 0)
+	{
+		cmd->failed = true;
+		return (perror(redir->name));
+	}
+	safe_close(&cmd->fd_in);
+	cmd->fd_out = redir->fd;
+}
+
+static void	handle_redir_append(t_cmd *cmd, t_redir_list *redir)
+{
+	if (redir->type != TOKEN_REDIR_APPEND)
+		return ;
+	redir->fd = open(redir->name, O_CREAT | O_WRONLY | O_APPEND, 0644);
+	if (redir->fd < 0)
+	{
+		cmd->failed = true;
+		return (perror(redir->name));
+	}
+	safe_close(&cmd->fd_in);
+	cmd->fd_out = redir->fd;
+}
