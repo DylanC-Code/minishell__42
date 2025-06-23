33afeca51163599f3a80e6242c1a4669bd9137ab
dcastor <dcastor@student.42.fr>
2025-06-23 10:15:43 +0200

    feat(executor): child exec handle exit code correctly

--- srcs/executor/child.c
+++ srcs/executor/child.c
@@ -6,11 +6,13 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 14:32:47 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/22 09:26:03 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/23 10:04:36 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	exec_or_died(t_app *app, t_cmd *cmd);
 
 void	child_exec(t_app *app, t_cmd *cmd)
 {
@@ -20,7 +22,52 @@ void	child_exec(t_app *app, t_cmd *cmd)
 		dup2(cmd->fd_out, STDOUT_FILENO);
 	safe_close(&cmd->fd_in);
 	safe_close(&cmd->fd_out);
+	exec_or_died(app, cmd);
+}
+
+char	*find_in_path(t_app *app, const char *cmd)
+{
+	char	*path;
+	char	*env_path;
+	char	**paths;
+	int		i;
+
+	env_path = get_env_value(app->env_head, "PATH");
+	if (!env_path)
+		return (NULL);
+	paths = ft_split(env_path, ':');
+	if (!paths)
+		return (NULL);
+	i = 0;
+	while (paths[i])
+	{
+		path = ft_strjoin(paths[i], "/");
+		if (!path)
+			return (NULL);
+		path = ft_strjoin(path, cmd);
+		if (!path)
+			return (NULL);
+		if (!access(path, X_OK))
+			break ;
+		i++;
+	}
+	return (path);
+}
+
+void	exec_or_died(t_app *app, t_cmd *cmd)
+{
+	char	*path;
+
 	if (!access(cmd->args[0], X_OK))
+	{
 		execve(cmd->args[0], cmd->args, env_list_to_envp(app->env_head,
 				&app->garb_head));
+		printf("minishell: %s: command not found\n", cmd->args[0]);
+		exit(127);
+		return ;
+	}
+	path = find_in_path(app, cmd->args[0]);
+	execve(path, cmd->args, env_list_to_envp(app->env_head, &app->garb_head));
+	printf("minishell: %s: command not found\n", cmd->args[0]);
+	exit(127);
 }
