c53584d2c045f6d71262f0585dca30b9e3ee04bc
dcastor <dcastor@student.42.fr>
2025-06-23 13:30:29 +0200

    refactor(executor): streamline logical operation checks in command execution

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,40 +6,35 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 15:24:44 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/23 10:09:01 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/23 13:19:26 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_status	init_exec(t_cmd_sequence *head_seq)
-{
-	if (!collect_heredocs(head_seq))
-		return (ERROR);
-	resolve_all_redirection(head_seq);
-	return (SUCCESS);
-}
+static bool	check_logical_op(t_cmd_sequence *seq);
 
-void	handle_exec(t_app *app, t_cmd_sequence *head_seq)
+void	handle_exec(t_app *app, t_cmd_sequence *head_seq, t_garbage **gc)
 {
 	if (!collect_heredocs(head_seq))
 		return ;
 	resolve_all_redirection(head_seq);
 	while (head_seq)
 	{
-		if (!setup_pipes(head_seq, &app->garb_head))
+		if (!setup_pipes(head_seq, gc))
 			return ;
 		exec_sequence(app, head_seq);
-		if (head_seq->logical_op == LOGICAL_AND)
-		{
-			if (head_seq->last_exit_status != 0)
-				break ;
-		}
-		else if (head_seq->logical_op == LOGICAL_OR)
-		{
-			if (head_seq->last_exit_status == 0)
-				break ;
-		}
+		if (!check_logical_op(head_seq))
+			return ;
 		head_seq = head_seq->next;
 	}
+}
+
+static bool	check_logical_op(t_cmd_sequence *seq)
+{
+	if (seq->logical_op == LOGICAL_AND && seq->last_exit_status == 0)
+		return (true);
+	if (seq->logical_op == LOGICAL_OR && seq->last_exit_status != 0)
+		return (true);
+	return (false);
 }
