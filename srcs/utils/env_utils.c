e465d9b1e019a0b644ab5c5e6bdbde2fa74a46f9
dcastor <dcastor@student.42.fr>
2025-06-23 15:50:52 +0200

    refactor(env_utils): implement set_env_value and add_env_node functions for environment variable management

--- srcs/utils/env_utils.c
+++ srcs/utils/env_utils.c
@@ -6,11 +6,13 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 09:48:22 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/23 09:40:54 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/23 14:01:36 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void		add_env_node(t_app *app, const char *key, const char *value);
 
 static char	*build_env_line(t_env *node, t_garbage **gc)
 {
@@ -64,4 +66,53 @@ char	*get_env_value(t_env *env_list, const char *key)
 		tmp = tmp->next;
 	}
 	return (NULL);
+}
+
+void	set_env_value(t_app *app, const char *key, const char *value)
+{
+	t_env	*tmp;
+
+	tmp = app->env_head;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->key, key) == 0)
+		{
+			tmp->value = ft_strdup(value);
+			return ;
+		}
+		tmp = tmp->next;
+	}
+	add_env_node(app, key, value);
+}
+
+void	add_env_node(t_app *app, const char *key, const char *value)
+{
+	t_env	*new_env;
+
+	new_env = gc_malloc(sizeof(t_env), &app->app_gc);
+	if (!new_env)
+		return ;
+	new_env->key = ft_strdup(key);
+	if (!new_env->key)
+		return ;
+	new_env->value = ft_strdup(value);
+	if (!new_env->value)
+		return ;
+	new_env->next = NULL;
+	add_env_back(&app->env_head, new_env);
+}
+
+void	add_env_back(t_env **env_list, t_env *new_env)
+{
+	t_env	*next;
+
+	if (!*env_list)
+	{
+		*env_list = new_env;
+		return ;
+	}
+	next = *env_list;
+	while (next->next)
+		next = next->next;
+	next->next = new_env;
 }
