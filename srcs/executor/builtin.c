d02101a2f20072586dd95a06d16819d4c331dd97
DylanC-Code <dylanc.code@gmail.com>
2025-06-25 16:25:23 +0200

    refactor(builtin): improve output redirection handling in exec_builtin

--- srcs/executor/builtin.c
+++ srcs/executor/builtin.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 09:10:41 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/25 15:52:00 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/25 16:24:58 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@ bool	is_builtin(const char *cmd)
 
 void	exec_builtin(t_app *app, t_cmd *cmd)
 {
+	int	saved_stdout;
+
+	if (cmd->fd_out != STDOUT_FILENO)
+	{
+		saved_stdout = dup(STDOUT_FILENO);
+		if (dup2(cmd->fd_out, STDOUT_FILENO) < 0)
+			perror("dup2");
+	}
 	// if (ft_strcmp(cmd->args[0], "cd") == 0)
 	// 	cd_builtin(app, cmd->args + 1);
 	// else if (ft_strcmp(cmd->args[0], "echo") == 0)
@@ -34,4 +42,11 @@ void	exec_builtin(t_app *app, t_cmd *cmd)
 	// 	pwd_builtin(app, cmd->args + 1);
 	// else if (ft_strcmp(cmd->args[0], "unset") == 0)
 	// 	unset_builtin(app, cmd->args + 1);
+	if (cmd->fd_out != STDOUT_FILENO)
+	{
+		safe_close(&cmd->fd_out);
+		safe_close(&cmd->fd_in);
+		if (dup2(saved_stdout, STDOUT_FILENO) < 0)
+			perror("dup2");
+	}
 }
