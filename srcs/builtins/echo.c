1b75f1dedbbdbe4f29031fad0500eba1b1afe64d
DylanC-Code <dylanc.code@gmail.com>
2025-06-25 11:37:56 +0200

    fix(echo): correct typo in write function call

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 12:09:37 by saal-kur          #+#    #+#             */
-/*   Updated: 2025/06/23 11:40:14 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/25 10:13:14 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	buitin_echo(t_app *app, char *str, int new_line)
 {
 	write(1, str, ft_strlen(str));
 	if (new_line == 1)
-		writet(1, "\n", 1);
+		write(1, "\n", 1);
 }
