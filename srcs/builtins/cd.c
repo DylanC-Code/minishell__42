83f84e54d411b59bc9c4c02e8f64363eb51daeae
Samy Al Kurdi <saal-kur@f1r5s6.clusters.42paris.fr>
2025-06-26 18:37:21 +0200

    cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 12:26:36 by saal-kur          #+#    #+#             */
-/*   Updated: 2025/06/26 17:15:35 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/27 10:14:06 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 void	cd_builtin(t_app *app, char **args)
 {
-	printf("CD BUILTIN NOT DONE!\n");
-	(void)app;
-	(void)args;
-	// if (!chdir((const char *)s))
-	// 	return (-1);
+	if (*(args + 1) != NULL)
+		print_error(app, "cd: too many arguments\n", "1");
+	else if (access(*args, R_OK) == -1)
+	{
+		perror("cd");
+		cleanup_and_exit(app, errno);
+	}
+	else if (access(*args, F_OK) == -1)
+	{
+		perror("cd");
+		cleanup_and_exit(app, errno);
+	}
 }
