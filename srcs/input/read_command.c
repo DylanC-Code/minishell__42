405b1263824f2b597b79dd520ebfbf9a59697401
dcastor <dcastor@student.42.fr>
2025-06-26 16:40:30 +0200

    refactor(read_command): improve error handling in get_user_input function

--- srcs/input/read_command.c
+++ srcs/input/read_command.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 22:26:29 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/26 16:05:36 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/26 16:37:20 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ t_token	*read_complete_command(t_app *app)
 	char	*line;
 	t_token	*token_head;
 
-	set_env_value(app, "?", "0");
 	line = get_user_input(app);
+	set_env_value(app, "?", "0");
 	token_head = tokenizer(app, line);
 	if (!token_head)
 		cleanup_and_exit(app, EXIT_FAILURE);
@@ -35,10 +35,10 @@ char	*get_user_input(t_app *app)
 
 	prompt = get_prompt(app);
 	if (!prompt)
-		return (NULL);
+		cleanup_and_exit(app, EXIT_FAILURE);
 	line = readline(prompt);
 	if (!line)
-		(cleanup(app), exit(EXIT_SUCCESS));
+		cleanup_and_exit(app, EXIT_SUCCESS);
 	add_history(line);
 	add_to_gc(&app->curr_gc, line);
 	return (line);
