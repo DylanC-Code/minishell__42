381f3a2877e8196140dc5a8435a0e92452c2cd56
phoenix <phoenix@ThinkPad.lan>
2025-06-26 09:30:52 +0200

    expansion w/ mixed quotes handling

--- srcs/executor/expansion.c
+++ srcs/executor/expansion.c
@@ -6,68 +6,25 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 12:45:36 by saal-kur          #+#    #+#             */
-/*   Updated: 2025/06/25 15:21:43 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/25 10:11:23 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	is_quoted(char *s, char quote_char)
-{
-	int	len;
-
-	len = ft_strlen(s);
-	if (len < 2)
-		return (0);
-	return (s[0] == quote_char && s[len - 1] == quote_char);
-}
-
-char	*remove_quotes(char *s, t_garbage **gc)
-{
-	int	len;
-
-	len = ft_strlen(s);
-	if (len < 2)
-		return (ft_strdup(s, gc));
-	return (ft_strndup(s + 1, len - 2, gc));
-}
-
-
 int	valid_env_start(char c)
 {
-	return ((c >= 'a' && c <= 'z') ||
-			(c >= 'A' && c <= 'Z') ||
-			c == '_');
+	return ((c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z')
+		|| c == '_');
 }
 
 int	valid_var_char(char c)
 {
-	return ((c >= 'a' && c <= 'z') ||
-			(c >= 'A' && c <= 'Z') ||
-			(c >= '0' && c <= '9') ||
-			c == '_');
-}
-
-char	*is_env_var(char *s, t_garbage **gc)
-{
-	int	i;
-	int	start;
-
-	start = 0;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == '$' && valid_env_start(s[i + 1]))
-		{
-			i++;
-			start = i;
-			while (s[i] && valid_var_char(s[i]))
-				i++;
-			return (ft_strndup(s + start, i - start, gc));
-		}
-		i++;
-	}
-	return (NULL);
+	return ((c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9')
+		|| c == '_');
 }
 
 char	*look_up_env(char *var_name, t_env *env)
@@ -84,55 +41,127 @@ char	*look_up_env(char *var_name, t_env *env)
 	return ("");
 }
 
-char	*resolved_env(char *cmd, char *var_name, char *env_value,
-		t_garbage **gc)
+int	find_quote_end(char *s, int start, char quote_char)
 {
-	char	*res;
-	int		i;
-	int		var_len;
+	int	i;
 
-	i = 0;
-	res = NULL;
-	var_len = ft_strlen(var_name);
-	while (cmd[i] && cmd[i] != '$')
+	i = start + 1;
+	while (s[i] && s[i] != quote_char)
 		i++;
-	if (i > 0)
-		res = ft_strndup(cmd, i, gc);
-	else
-		res = ft_strdup("", gc);
-	res = ft_strjoin(res, env_value, gc);
-	res = ft_strjoin(res, cmd + i + 1 + var_len, gc);
-	return (res);
+	return (i);
 }
 
+char	*build_var(char *s, int *i, t_app *app)
+{
+	char	*result;
+	char	*var_name;
+	char	*value;
+	int		var_start;
+
+	result = ft_strndup(s, *i, &app->curr_gc);
+	var_start = *i + 1;
+	(*i)++;
+	while (s[*i] && valid_var_char(s[*i]))
+		(*i)++;
+	var_name = ft_strndup(s + var_start, *i - var_start, &app->curr_gc);
+	value = look_up_env(var_name, app->env_head);
+	result = ft_strjoin(result, value, &app->curr_gc);
+	return (ft_strjoin(result, s + *i, &app->curr_gc));
+}
+
+char	*process_variable_expansion(char *section, t_app *app)
+{
+	int	i;
+
+	i = 0;
+	while (section[i])
+	{
+		if (section[i] == '$' && valid_env_start(section[i + 1]))
+			return (build_var(section, &i, app));
+		i++;
+	}
+	return (ft_strdup(section, &app->curr_gc));
+}
+
+char	*process_section_literal(char *s, int start, int end, t_app *app)
+{
+	return (ft_strndup(s + start, end - start, &app->curr_gc));
+}
+
+char	*process_section_expand(char *s, int start, int end, t_app *app)
+{
+	char	*section;
+
+	section = ft_strndup(s + start, end - start, &app->curr_gc);
+	return (process_variable_expansion(section, app));
+}
+
+char	*handle_quoted_part(char *arg, int *i, t_app *app)
+{
+	char	quote_char;
+	int		end;
+
+	quote_char = arg[*i];
+	end = find_quote_end(arg, *i, quote_char);
+	if (quote_char == '"')
+		return (process_section_expand(arg, *i + 1, end, app));
+	return (process_section_literal(arg, *i + 1, end, app));
+}
+
+char	*handle_unquoted_part(char *arg, int start, int *i, t_app *app)
+{
+	while (arg[*i] && arg[*i] != '"' && arg[*i] != '\'')
+		(*i)++;
+	return (process_section_expand(arg, start, *i, app));
+}
+
+char	*process_mixed_quotes(char *arg, t_app *app)
+{
+	char	*result;
+	char	*part;
+	int		i;
+	int		start;
+
+	result = ft_strdup("", &app->curr_gc);
+	i = 0;
+	while (arg[i])
+	{
+		if (arg[i] == '"' || arg[i] == '\'')
+		{
+			part = handle_quoted_part(arg, &i, app);
+			i = find_quote_end(arg, i, arg[i]) + 1;
+		}
+		else
+		{
+			start = i;
+			part = handle_unquoted_part(arg, start, &i, app);
+		}
+		result = ft_strjoin(result, part, &app->curr_gc);
+	}
+	return (result);
+}
+
+int	contains_quotes(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '"' || s[i] == '\'')
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 char	*process_argument(char *arg, t_app *app)
 {
-	char	*var_name;
-	char	*value;
-	char	*unquoted;
-
-	if (is_quoted(arg, '\''))
-	return (remove_quotes(arg, &app->curr_gc));
-	if (is_quoted(arg, '"'))
-	{
-		unquoted = remove_quotes(arg, &app->curr_gc);
-		var_name = is_env_var(unquoted, &app->app_gc);
-		if (var_name)
-		{
-			value = look_up_env(var_name, app->env_head);
-			return (resolved_env(unquoted, var_name, value, &app->curr_gc));
-		}
-		return (unquoted);
-	}
-	var_name = is_env_var(arg, &app->app_gc);
-	if (var_name)
-	{
-		value = look_up_env(var_name, app->env_head);
-		return (resolved_env(arg, var_name, value, &app->curr_gc));
-	}
-	return (ft_strdup(arg, &app->curr_gc));
+	if (contains_quotes(arg))
+		return (process_mixed_quotes(arg, app));
+	return (process_variable_expansion(arg, app));
 }
+
 void	traverse_cmd(t_cmd *cmd_head, t_app *app)
 {
 	t_cmd	*curr;
