8c9dd0014f373bf09449ec0566d4a210c3351fc9
dcastor <dcastor@student.42.fr>
2025-06-26 19:34:57 +0200

    refactor(exit): implement exit_builtin function with error handling for non-numeric arguments

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,16 +6,37 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:56:46 by dcastor           #+#    #+#             */
-/*   Updated: 2025/06/26 17:13:49 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/06/26 18:17:28 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	non_numeric_arg_error(t_app *app, char *arg);
+
 void	exit_builtin(t_app *app, char **args)
 {
-	printf("EXIT BUILTIN NOT DONE!\n");
-	(void)args;
-	gc_cleanup(&app->app_gc);
-	// exit(status);
+	char	status_code;
+
+	printf("exit\n");
+	if (!is_valid_number(*args))
+		non_numeric_arg_error(app, *args);
+	else if (args[1])
+		return (print_error(app, "exit:  too many arguments\n", "1"));
+	status_code = ft_atoi(*args);
+	cleanup_and_exit(app, status_code);
+}
+
+static void	non_numeric_arg_error(t_app *app, char *arg)
+{
+	char	*msg;
+
+	msg = ft_strjoin("exit: ", arg, &app->app_gc);
+	if (!msg)
+		cleanup_and_exit(app, EXIT_FAILURE);
+	msg = ft_strjoin(msg, ": numeric argument required\n", &app->app_gc);
+	if (!msg)
+		cleanup_and_exit(app, EXIT_FAILURE);
+	print_error(app, msg, "2");
+	cleanup_and_exit(app, 2);
 }
