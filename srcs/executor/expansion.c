ba8ff94f232dcd11eaad1ed719c7a62d0073ab28
phoenix <phoenix@ThinkPad.lan>
2025-07-01 17:24:08 +0200

    fixed expansion

--- srcs/executor/expansion.c
+++ srcs/executor/expansion.c
@@ -51,36 +51,76 @@ int	find_quote_end(char *s, int start, char quote_char)
 	return (i);
 }
 
-char	*build_var(char *s, int *i, t_app *app)
+char	*add_text_part(char *str, int start, int end, t_app *app)
 {
-	char	*result;
+	char	*temp;
+
+	if (start >= end)
+		return ("");
+	temp = ft_strndup(str + start, end - start, &app->curr_gc);
+	return (temp);
+}
+
+char	*get_var_value(char *str, int start, int end, t_app *app)
+{
 	char	*var_name;
 	char	*value;
-	int		var_start;
 
-	result = ft_strndup(s, *i, &app->curr_gc);
-	var_start = *i + 1;
-	(*i)++;
-	while (s[*i] && valid_var_char(s[*i]))
-		(*i)++;
-	var_name = ft_strndup(s + var_start, *i - var_start, &app->curr_gc);
+	var_name = ft_strndup(str + start, end - start, &app->curr_gc);
 	value = look_up_env(var_name, app->env_head);
-	result = ft_strjoin(result, value, &app->curr_gc);
-	return (ft_strjoin(result, s + *i, &app->curr_gc));
+	return (value);
+}
+
+int	find_var_end(char *str, int start)
+{
+	int	i;
+
+	i = start;
+	while (str[i] && valid_var_char(str[i]))
+		i++;
+	return (i);
+}
+
+char	*join_parts(char *result, char *part, t_app *app)
+{
+	char	*new_result;
+
+	new_result = ft_strjoin(result, part, &app->curr_gc);
+	return (new_result);
+}
+
+char	*build_vars(char *str, t_app *app)
+{
+	char	*result;
+	char	*part;
+	int		pos;
+	int		last_pos;
+
+	result = ft_strdup("", &app->curr_gc);
+	pos = 0;
+	last_pos = 0;
+	while (str[pos])
+	{
+		if (str[pos] == '$' && valid_env_start(str[pos + 1]))
+		{
+			part = add_text_part(str, last_pos, pos, app);
+			result = join_parts(result, part, app);
+			last_pos = ++pos;
+			pos = find_var_end(str, pos);
+			part = get_var_value(str, last_pos, pos, app);
+			result = join_parts(result, part, app);
+			last_pos = pos;
+		}
+		else
+			pos++;
+	}
+	part = add_text_part(str, last_pos, pos, app);
+	return (join_parts(result, part, app));
 }
 
 char	*process_variable_expansion(char *section, t_app *app)
 {
-	int	i;
-
-	i = 0;
-	while (section[i])
-	{
-		if (section[i] == '$' && valid_env_start(section[i + 1]))
-			return (build_var(section, &i, app));
-		i++;
-	}
-	return (ft_strdup(section, &app->curr_gc));
+	return (build_vars(section, app));
 }
 
 char	*process_section_literal(char *s, int start, int end, t_app *app)
