a482e159fbe120ff30abda1d39583521485af0f8
phoenix <phoenix@ThinkPad.lan>
2025-07-03 13:14:14 +0200

    $text fix + protections mallocs

--- srcs/executor/expansion.c
+++ srcs/executor/expansion.c
@@ -59,6 +59,8 @@ char	*add_text_part(char *str, int start, int end, t_app *app)
 	if (start >= end)
 		return ("");
 	temp = ft_strndup(str + start, end - start, &app->curr_gc);
+	if(!temp)
+		cleanup_and_exit(app, EXIT_FAILURE);
 	return (temp);
 }
 
@@ -68,6 +70,8 @@ char	*get_var_value(char *str, int start, int end, t_app *app)
 	char	*value;
 
 	var_name = ft_strndup(str + start, end - start, &app->curr_gc);
+	if(!var_name)
+		cleanup_and_exit(app, EXIT_FAILURE);
 	value = look_up_env(var_name, app->env_head);
 	return (value);
 }
@@ -89,9 +93,48 @@ char	*join_parts(char *result, char *part, t_app *app)
 	char	*new_result;
 
 	new_result = ft_strjoin(result, part, &app->curr_gc);
+	if(!new_result)
+		cleanup_and_exit(app, EXIT_FAILURE);
 	return (new_result);
 }
 
+char	*handle_dollar_quote(char *str, int *pos, t_app *app)
+{
+	char	*part;
+	int		last_pos;
+
+	(*pos)++;
+	(*pos)++;
+	last_pos = *pos;
+	while (str[*pos] && str[*pos] != '"')
+		(*pos)++;
+	part = ft_strndup(str + last_pos, *pos - last_pos, &app->curr_gc);
+	if(!part)
+		cleanup_and_exit(app, EXIT_FAILURE);
+	part = process_variable_expansion(part, app);
+	if (str[*pos] == '"')
+		(*pos)++;
+	return (part);
+}
+
+char	*handle_regular_var(char *str, int *pos, t_app *app)
+{
+	char	*part;
+	int		last_pos;
+
+	last_pos = ++(*pos);
+	*pos = find_var_end(str, *pos);
+	part = get_var_value(str, last_pos, *pos, app);
+	return (part);
+}
+void	set_build_vars_variables(int *pos, int *last_pos, char **res, t_app *app)
+{
+	*pos = 0;
+	*last_pos = 0;
+	*res = ft_strdup("", &app->curr_gc);
+	if(!res)
+		cleanup_and_exit(app, EXIT_FAILURE);
+}
 char	*build_vars(char *str, t_app *app)
 {
 	char	*result;
@@ -99,18 +142,18 @@ char	*build_vars(char *str, t_app *app)
 	int		pos;
 	int		last_pos;
 
-	result = ft_strdup("", &app->curr_gc);
-	pos = 0;
-	last_pos = 0;
+	set_build_vars_variables(&pos, &last_pos, &result, app);
 	while (str[pos])
 	{
-		if (str[pos] == '$' && valid_env_start(str[pos + 1]))
+		if (str[pos] == '$' && str[pos + 1] && (valid_env_start(str[pos + 1]) 
+			|| str[pos + 1] == '"'))
 		{
 			part = add_text_part(str, last_pos, pos, app);
 			result = join_parts(result, part, app);
-			last_pos = ++pos;
-			pos = find_var_end(str, pos);
-			part = get_var_value(str, last_pos, pos, app);
+			if (str[pos + 1] == '"')
+				part = handle_dollar_quote(str, &pos, app);
+			else
+				part = handle_regular_var(str, &pos, app);
 			result = join_parts(result, part, app);
 			last_pos = pos;
 		}
@@ -128,7 +171,12 @@ char	*process_variable_expansion(char *section, t_app *app)
 
 char	*process_section_literal(char *s, int start, int end, t_app *app)
 {
-	return (ft_strndup(s + start, end - start, &app->curr_gc));
+	char	*res;
+
+	res = ft_strndup(s + start, end - start, &app->curr_gc);
+	if(!res)
+		cleanup_and_exit(app, EXIT_FAILURE);
+	return (res);
 }
 
 char	*process_section_expand(char *s, int start, int end, t_app *app)
@@ -136,6 +184,8 @@ char	*process_section_expand(char *s, int start, int end, t_app *app)
 	char	*section;
 
 	section = ft_strndup(s + start, end - start, &app->curr_gc);
+	if(!section)
+		cleanup_and_exit(app, EXIT_FAILURE);
 	return (process_variable_expansion(section, app));
 }
 
@@ -151,13 +201,80 @@ char	*handle_quoted_part(char *arg, int *i, t_app *app)
 	return (process_section_literal(arg, *i + 1, end, app));
 }
 
-char	*handle_unquoted_part(char *arg, int start, int *i, t_app *app)
+char	*add_text_before_dollar(char *result, char *arg, int last_pos, int pos, t_app *app)
 {
-	while (arg[*i] && arg[*i] != '"' && arg[*i] != '\'')
-		(*i)++;
-	return (process_section_expand(arg, start, *i, app));
+	char	*part;
+
+	if (pos > last_pos)
+	{
+		part = ft_strndup(arg + last_pos, pos - last_pos, &app->curr_gc);
+		if(!part)
+			cleanup_and_exit(app, EXIT_FAILURE);
+		part = process_variable_expansion(part, app);
+		result = ft_strjoin(result, part, &app->curr_gc);
+		if(!result)
+			cleanup_and_exit(app, EXIT_FAILURE);
+	}
+	return (result);
 }
 
+char	*process_unquoted_text(char *arg, int start, int end, t_app *app)
+{
+	char	*result;
+	char	*part;
+	int		pos;
+	int		last_pos;
+
+	result = ft_strdup("", &app->curr_gc);
+	if(!result)
+		cleanup_and_exit(app, EXIT_FAILURE);
+	pos = start;
+	last_pos = start;
+	while (pos < end)
+	{
+		if (arg[pos] == '$' && pos + 1 < end && arg[pos + 1] == '"')
+		{
+			result = add_text_before_dollar(result, arg, last_pos, pos, app);
+			part = handle_dollar_quote(arg, &pos, app);
+			result = ft_strjoin(result, part, &app->curr_gc);
+			if(!result)
+				cleanup_and_exit(app, EXIT_FAILURE);
+			last_pos = pos;
+		}
+		else
+			pos++;
+	}
+	return (add_text_before_dollar(result, arg, last_pos, end, app));
+}
+
+char	*handle_unquoted_part(char *arg, int start, int *i, t_app *app)
+{
+	int	end;
+
+	end = start;
+	while (arg[end] && arg[end] != '"' && arg[end] != '\'')
+	{
+		if (arg[end] == '$' && arg[end + 1] == '"')
+		{
+			end += 2;
+			while (arg[end] && arg[end] != '"')
+				end++;
+			if (arg[end] == '"')
+				end++;
+		}
+		else
+			end++;
+	}
+	*i = end;
+	return (process_unquoted_text(arg, start, end, app));
+}
+void	set_mixed_quotes_vars(t_app *app, char **res, int *i)
+{
+	*i = 0;
+	*res = ft_strdup("", &app->curr_gc);
+	if(!res)
+		cleanup_and_exit(app, EXIT_FAILURE);
+}
 char	*process_mixed_quotes(char *arg, t_app *app)
 {
 	char	*result;
@@ -165,8 +282,7 @@ char	*process_mixed_quotes(char *arg, t_app *app)
 	int		i;
 	int		start;
 
-	result = ft_strdup("", &app->curr_gc);
-	i = 0;
+	set_mixed_quotes_vars(app, &result, &i);
 	while (arg[i])
 	{
 		if (arg[i] == '"' || arg[i] == '\'')
@@ -180,6 +296,8 @@ char	*process_mixed_quotes(char *arg, t_app *app)
 			part = handle_unquoted_part(arg, start, &i, app);
 		}
 		result = ft_strjoin(result, part, &app->curr_gc);
+		if(!result)
+			cleanup_and_exit(app, EXIT_FAILURE);
 	}
 	return (result);
 }
